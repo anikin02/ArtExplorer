bda703f86c491aba505fc6021397ef000934a1b9
Danil Anikin <mart4r.clearlist@gmail.com>
2024-04-01 11:09:52 +1000

    Initial Commit

---
+++ ArtExplorer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtExplorer
+//
+//  Created by Данил Аникин on 01.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
