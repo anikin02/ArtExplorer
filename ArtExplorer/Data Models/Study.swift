33b0775b09936ad56f5d09264e0a7534f032cffe
Danil Anikin <mart4r.clearlist@gmail.com>
2024-04-08 21:49:57 +1000

    Added new model (Study)

---
+++ ArtExplorer/Data Models/Study.swift
@@ -0,0 +1,21 @@
+//
+//  Study.swift
+//  ArtExplorer
+//
+//  Created by anikin02 on 08.04.2024.
+//
+
+import Foundation
+
+
+class Study {
+  var name: String
+  var author: String
+  var text: String
+  
+  init(name: String, author: String, text: String) {
+    self.name = name
+    self.author = author
+    self.text = text
+  }
+}
