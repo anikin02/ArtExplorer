3a09b6390ce8d1875961b04f5fa1c1bff5bebb44
Danil Anikin <mart4r.clearlist@gmail.com>
2024-04-08 22:09:24 +1000

    Updated StudyViewController

--- ArtExplorer/View Contollers/StudyViewController.swift
+++ ArtExplorer/View Contollers/StudyViewController.swift
@@ -7,21 +7,76 @@
 
 import UIKit
 
-class StudyViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class StudyViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
   
   private var tableView = UITableView()
+  private let searchBar = UISearchBar()
+  private var studyCollection = [Study]()
 
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    view.backgroundColor = .white
+    
+    // TEST DATA
+    for i in 0...10 {
+      studyCollection.append(Study(name: "Название \(i)", author: "Автор \(i)", text: ""))
+    }
+    //
+    
+    generateSafeArea()
+    generateSearchBar()
+    generateTableView()
+    
+  }
+  
+  private func generateSafeArea() {
+    let safeAreaView = UIView()
+    safeAreaView.backgroundColor = .white
+
+    view.addSubview(safeAreaView)
+    safeAreaView.snp.makeConstraints { maker in
+      maker.top.equalTo(view.snp.top)
+      maker.left.right.equalToSuperview()
+      maker.bottom.equalTo(view.safeAreaLayoutGuide.snp.top)
+    }
+  }
+  
+  private func generateSearchBar() {
+    searchBar.delegate = self
+    searchBar.placeholder = "Search..."
+    view.addSubview(searchBar)
+    
+    searchBar.snp.makeConstraints { maker in
+      maker.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+      maker.left.right.equalToSuperview()
+    }
+  }
+  
+  private func generateTableView() {
+    tableView.delegate = self
+    tableView.dataSource = self
+    tableView.allowsSelection = false
+    tableView.showsVerticalScrollIndicator = false
+    view.addSubview(tableView)
+    tableView.snp.makeConstraints { maker in
+      maker.top.equalTo(searchBar.snp.bottom)
+      maker.left.right.bottom.equalToSuperview()
+    }
   }
   
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 0
+    return studyCollection.count
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    return UITableViewCell()
+    let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+        
+    cell.textLabel?.text = studyCollection[indexPath.row].name
+    
+    cell.detailTextLabel?.text = studyCollection[indexPath.row].author
+    
+    return cell
   }
 
 }
